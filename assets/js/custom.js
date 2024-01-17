c1087307b2d1dd1d3055d49bfe5790fb2a9488d3
viralsolanki <viralsolanki2@gmail.com>
2024-01-17 15:27:59 +0530

    create home and gallery page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -20,3 +20,31 @@ $(function () { // Dropdown toggle
     { $('.dropdown-menu').slideUp(); }
   });
 });
+
+
+const swiperTestmonials = new Swiper('.swiper-testmonials', {
+  loop: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-testmonials-next',
+    prevEl: '.swiper-button-testmonials-prev',
+  },
+  breakpoints: {
+    // when window width is >= 640px
+    500: {
+      slidesPerView: 1.2,
+    },
+    780: {
+      slidesPerView: 1.8,
+    },
+    1300: {
+      slidesPerView: 2.6,
+    },
+    1630: {
+      slidesPerView: 2.2,
+    }
+  }
+
+});
